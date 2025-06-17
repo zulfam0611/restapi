debd2acf7dbaf04047589ca5bcdb3d5b7a1d71e7
Zulfa Muqsithoh <zulfamuqsithoh06@gmail.com>
2025-06-17 22:51:33 +0700

    yoooo

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {admin } = require('../firebase-config');
+const { admin } = require('../firebase-config');
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
